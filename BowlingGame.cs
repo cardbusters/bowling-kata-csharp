a0dcb20e145c633360fcb2dfbc32646b0273887c
slange <slange@igus.net>
2022-01-27 14:05:44 +0100

    refactor: replaced Tuple with Frame class
    Co-authored-by: Alexey Tolkov <atolkov.ext@igus.net>
    Co-authored-by: Tugbay Atilla <tugbayatilla@hotmail.com>

--- BowlingGame.cs
+++ BowlingGame.cs
@@ -6,7 +6,8 @@ namespace bowling_kata_csharp1
 {
     internal class BowlingGame
     {
-        private List<Tuple<int, int, int>> _score = new List<Tuple<int, int, int>>();
+
+        private List<Frame> _score = new List<Frame>();
         internal void OpenFrame(int firstThrow, int secondThrow)
         {
             int totalFrame = firstThrow + secondThrow;
@@ -15,7 +16,7 @@ namespace bowling_kata_csharp1
             {
                 throw new ArgumentOutOfRangeException();
             }
-            _score.Add(new Tuple<int, int, int>(firstThrow, secondThrow, totalFrame));
+            _score.Add(new Frame(firstThrow, secondThrow, totalFrame));
         }
 
         internal int Score()
@@ -25,15 +26,15 @@ namespace bowling_kata_csharp1
                 if (IsStrike(1))
                 {
                     var newScore = _score[0].Item3 + _score[1].Item3 + _score[2].Item1;
-                    _score[0] = new Tuple<int, int, int>(_score[0].Item1, _score[0].Item2, newScore);
+                    _score[0] = new Frame(_score[0].Item1, _score[0].Item2, newScore);
 
                     newScore = _score[1].Item3 + _score[2].Item3;
-                    _score[1] = new Tuple<int, int, int>(_score[1].Item1, _score[1].Item2, newScore);
+                    _score[1] = new Frame(_score[1].Item1, _score[1].Item2, newScore);
                 }
                 else
                 {
                     var newScore = _score[0].Item3 + _score[1].Item3;
-                    _score[0] = new Tuple<int, int, int>(_score[0].Item1, _score[0].Item2, newScore);
+                    _score[0] = new Frame(_score[0].Item1, _score[0].Item2, newScore);
                 }
             }
 
@@ -43,6 +44,20 @@ namespace bowling_kata_csharp1
         private bool IsStrike(int frameIndex)
         {
             return _score[frameIndex].Item1 == _score[frameIndex].Item3 && _score[frameIndex].Item3 == 10;
+        }
+
+        private class Frame
+        {
+            public int Item1;
+            public int Item2;
+            public int Item3;
+
+            public Frame(int firstThrow, int secondThrow, int totalFrame)
+            {
+                this.Item1 = firstThrow;
+                this.Item2 = secondThrow;
+                this.Item3 = totalFrame;
+            }
         }
     }
 }

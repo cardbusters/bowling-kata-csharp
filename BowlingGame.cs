d3feef8aca3d2113ae639c57ac835eba00da0541
Tugbay Atilla <tugbayatilla@gmail.com>
2022-02-24 13:52:52 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- BowlingGame.cs
+++ BowlingGame.cs
@@ -7,16 +7,36 @@ namespace bowling_kata_csharp1
     internal class BowlingGame
     {
 
-        private List<Frame> _frameHistory = new List<Frame>();
+        private List<IFrame> _frameHistory = new List<IFrame>();
         internal void OpenFrame(int firstThrow, int secondThrow)
         {
-            var frame = Frame.initializeFrame(firstThrow, secondThrow);
-            _frameHistory.Add(frame);
+            int totalFrame = firstThrow + secondThrow;
+
+            if (totalFrame > 10)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            IFrame frame;
+            if (firstThrow == 10)
+            {
+                frame = new StrikeFrame(firstThrow, secondThrow, totalFrame);
+                _frameHistory.Add(frame);
+            }
+            else if (firstThrow != 10 && totalFrame == 10){
+                frame = new SpareFrame(firstThrow, secondThrow, totalFrame);
+                _frameHistory.Add(frame);
+            }
+            else
+            {
+                frame = new Frame(firstThrow, secondThrow, totalFrame);
+                _frameHistory.Add(frame);
+            }
         }
 
         internal int Score()
         {
-            for(int i = 0; i < _frameHistory.Count; i++)
+            for (int i = 0; i < _frameHistory.Count; i++)
             {
                 RecalculateFrames(i);
             }
@@ -26,17 +46,17 @@ namespace bowling_kata_csharp1
 
         private void RecalculateFrames(int frameIndex)
         {
-            if (_frameHistory[frameIndex].IsStrike()) //first throw is strike
+            if (_frameHistory[frameIndex] is StrikeFrame) //first throw is strike
             {
-                if (_frameHistory.Count > frameIndex + 1 )
+                if (_frameHistory.Count > frameIndex + 1)
                 {
-                    if (_frameHistory[frameIndex + 1].IsStrike()) //second throw is also strike
+                    if (_frameHistory[frameIndex + 1] is StrikeFrame) //second throw is also strike
                     {
 
                         var newScore = _frameHistory[frameIndex].Score + _frameHistory[frameIndex + 1].Score + _frameHistory[frameIndex + 2].ThrowOne;
                         _frameHistory[frameIndex] = new Frame(_frameHistory[frameIndex].ThrowOne, _frameHistory[frameIndex].ThrowTwo, newScore);
 
-                        if(_frameHistory.Count > frameIndex + 2)
+                        if (_frameHistory.Count > frameIndex + 2)
                         {
                             newScore = _frameHistory[frameIndex + 1].Score + _frameHistory[frameIndex + 2].Score;
                             _frameHistory[frameIndex + 1] = new Frame(_frameHistory[frameIndex + 1].ThrowOne, _frameHistory[frameIndex + 1].ThrowTwo, newScore);
@@ -50,18 +70,53 @@ namespace bowling_kata_csharp1
                 }
             }
 
-            if (_frameHistory[frameIndex].IsSpare() && _frameHistory.Count > frameIndex + 1)
+            if (_frameHistory[frameIndex] is SpareFrame && _frameHistory.Count > frameIndex + 1)
             {
                 var newScore = _frameHistory[frameIndex].Score + _frameHistory[frameIndex + 1].ThrowOne;
                 _frameHistory[frameIndex] = new Frame(_frameHistory[frameIndex].ThrowOne, _frameHistory[frameIndex].ThrowTwo, newScore);
             }
         }
 
-        private class Frame
+        public abstract class IFrame
         {
-            public int ThrowOne;
-            public int ThrowTwo;
-            public int Score;
+            public int ThrowOne { get; }
+            public int ThrowTwo { get; }
+            public int Score { get; }
+        }
+
+        class StrikeFrame : IFrame
+        {
+            public int ThrowOne { get; }
+            public int ThrowTwo { get; }
+            public int Score { get; }
+
+            public StrikeFrame(int firstThrow, int secondThrow, int totalFrame)
+            {
+                this.ThrowOne = firstThrow;
+                this.ThrowTwo = secondThrow;
+                this.Score = totalFrame;
+            }
+        }
+
+        class SpareFrame : IFrame
+        {
+            public int ThrowOne { get; }
+            public int ThrowTwo { get; }
+            public int Score { get; }
+
+            public SpareFrame(int firstThrow, int secondThrow, int totalFrame)
+            {
+                this.ThrowOne = firstThrow;
+                this.ThrowTwo = secondThrow;
+                this.Score = totalFrame;
+            }
+        }
+
+        private class Frame : IFrame
+        {
+            public int ThrowOne { get; }
+            public int ThrowTwo { get; }
+            public int Score { get; }
 
             public Frame(int firstThrow, int secondThrow, int totalFrame)
             {
@@ -79,16 +134,6 @@ namespace bowling_kata_csharp1
                 return ThrowTwo > 0 && Score == 10;
             }
 
-            public static Frame initializeFrame(int firstThrow, int secondThrow)
-            {
-                int totalFrame = firstThrow + secondThrow;
-
-                if (totalFrame > 10)
-                {
-                    throw new ArgumentOutOfRangeException();
-                }
-                return new Frame(firstThrow, secondThrow, totalFrame);
-            }
         }
     }
 }
